b1c661bd688c928e757bbac7cb858c773f71498a
saylorsd <Hambone#25>
2016-07-19 23:25:16 -0400

    started file intake processes

--- combiner/views.py
+++ combiner/views.py
@@ -1,3 +1,31 @@
 from django.shortcuts import render
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
 
-# Create your views here.
+from .models import Document
+from .forms import DocumentForm
+
+
+def index(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile=request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('list'))
+    else:
+        form = DocumentForm()  # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.all()
+
+    # Render list page with the documents and the form
+    return render(
+        request,
+        'combiner/index.html',
+        {'documents': documents, 'form': form}
+    )
